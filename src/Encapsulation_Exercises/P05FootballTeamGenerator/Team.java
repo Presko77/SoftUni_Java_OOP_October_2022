db5aa16c87daf72536af5a27d9ed6f208ff9bbb4
Presko77 <presyanvelikov@gmail.com>
2022-11-01 22:43:21 +0200

    Update vcs.xml

---
+++ src/Encapsulation_Exercises/P05FootballTeamGenerator/Team.java
@@ -0,0 +1,62 @@
+package Encapsulation_Exercises.P05FootballTeamGenerator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Team {
+    private String name;
+    private List<Player> players;
+
+    public Team(String name) {
+        this.setName(name);
+        this.players = new ArrayList<>();
+    }
+
+    private void setName(String name) {
+        if (name == null || name.trim().isEmpty()) {
+            validateName();
+        }
+        this.name = name;
+    }
+
+    private void validateName() {
+        throw new IllegalArgumentException("A name should not be empty.");
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+//    public List<Player> getPlayers() {
+//        return Collections.unmodifiableList(this.players);
+//    }
+
+    public boolean hasPlayer(String name) {
+        boolean result = false;
+
+        for (Player player : this.players) {
+            if (player.getName().equals(name)) {
+                result = true;
+                break;
+            }
+        }
+
+        return result;
+    }
+
+    public void addPlayer(Player player) {
+        this.players.add(player);
+    }
+
+    public void removePlayer(String removePlayer) {
+        players.removeIf(player -> player.getName().equals(removePlayer));
+    }
+
+    public double getRating() {
+        return this.players.stream()
+                .mapToDouble(Player::overallSkillLevel)
+                .sum()/this.players.size();
+    }
+
+
+}
