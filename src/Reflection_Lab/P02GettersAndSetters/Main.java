231b195a350d9bd3fd6b2118af0e49d0b902a944
Presko77 <presyanvelikov@gmail.com>
2022-11-15 22:59:09 +0200

    Update vcs.xml

---
+++ src/Reflection_Lab/P02GettersAndSetters/Main.java
@@ -0,0 +1,39 @@
+package Reflection_Lab.P02GettersAndSetters;
+
+import java.lang.reflect.Method;
+import java.util.Comparator;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Main {
+    public static void main(String[] args){
+
+       Class<Reflection> clazz = Reflection.class;
+
+        Method[] methods = clazz.getDeclaredMethods();
+
+        Comparator<Method> comparator = Comparator.comparing(Method::getName);
+
+        Set<Method> getters = new TreeSet<>(comparator);
+        Set<Method> setters = new TreeSet<>(comparator);
+
+
+        for (Method method : methods) {
+            String methodName = method.getName();
+            if (methodName.contains("get")){
+                getters.add(method);
+            }else if (methodName.contains("set")){
+                setters.add(method);
+            }
+        }
+
+        for (Method getter : getters) {
+            System.out.println(getter.getName() + " will return class " + getter.getReturnType().getName());
+        }
+
+        for (Method setter : setters) {
+            System.out.println(setter.getName() + " and will set field of class " + setter.getParameterTypes()[0].getName());
+        }
+
+    }
+}
